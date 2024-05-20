ea010549da9bf700c54f37bbbf544dd4f1419816
spencerhcindia <107213392+spencerhcindia@users.noreply.github.com>
2024-05-20 14:34:47 -0400

    some more gulpdates, colors, dropdown menu? etc

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 // scripts.js
+document.getElementById('nav-toggle').addEventListener('click', function () {
+    const navMenu = document.querySelector('nav ul');
+    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
